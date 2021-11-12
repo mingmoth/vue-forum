88d0ea14ec1fc4ec96638446d1d7bf98810bd888
mingmoth <mingmoth@gmail.com>
2021-11-12 18:11:43 +0800

    modify vue router

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,12 @@ import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  
-  {
-    path: '*',
-    name: 'not-found',
-    component: NotFound
-  },
-]
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  routes : [
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound
+    },
+  ]
 })
-
-export default router
