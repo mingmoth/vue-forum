a427109e1b8eb2cbc00caee50bfb00e7e3eed420
mingmoth <mingmoth@gmail.com>
2021-11-13 14:22:55 +0800

    add 4 Main vue Page/NavTabs & install uuid

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
+  linkExactActiveClass: 'active',
   routes : [
     {
       path: '/signin',
@@ -16,6 +18,26 @@ export default new VueRouter({
       path: '/signup',
       name: 'sign-up',
       component: () => import("../views/SignUp.vue")
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      component: Restaurants
+    },
+    {
+      path: '/restaurants/feeds',
+      name: 'restaurants-feeds',
+      component: () => import ("../views/RestaurantsFeeds.vue")
+    },
+    {
+      path: '/restaurants/top',
+      name: 'restaurants-top',
+      component: () => import("../views/RestaruantsTop.vue")
+    },
+    {
+      path: '/users/top',
+      name: 'users=top',
+      component: () => import("../views/UsersTop.vue")
     },
     {
       path: '*',
---
+++ src/views/UsersTop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container py-5">
+    <NavTabs />
+    <h1 class="mt-5">
+      UsersTop
+    </h1>
+  </div>
+</template>
+
+<script>
+import NavTabs from '../components/NavTabs.vue'
+export default {
+  name: 'UsersTop',
+  components: {
+    NavTabs,
+  }
+}
+</script>
