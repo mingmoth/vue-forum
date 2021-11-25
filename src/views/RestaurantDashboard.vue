9b725e152922bb4c48e43ead77e134efa768d518
mingmoth <mingmoth@gmail.com>
2021-11-25 18:07:38 +0800

    add RestaurantDashboard API integration & beforeRouteUpdate

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -7,125 +7,68 @@
       </span>
     </div>
 
-    <hr>
+    <hr />
 
     <ul>
-      <li>評論數： {{ restaurant.commentsLength }} </li>
-      <li>瀏覽次數： {{ restaurant.viewCounts }} </li>
+      <li>評論數： {{ restaurant.commentsLength }}</li>
+      <li>瀏覽次數： {{ restaurant.viewCounts }}</li>
     </ul>
 
-    <button
-      type="button"
-      class="btn btn-link"
-      @click="$router.back()"
-    >
+    <button type="button" class="btn btn-link" @click="$router.back()">
       回上一頁
     </button>
   </div>
 </template>
 
 <script>
-const dummyData = {
-    "restaurant": {
-        "id": 1,
-        "name": "Macey Durgan Jr.",
-        "tel": "(048) 327-7674",
-        "address": "018 Kulas Extension",
-        "opening_hours": "08:00",
-        "description": "Totam distinctio quos quas mollitia et. Dolor et aut animi tempora et similique. Explicabo aut qui voluptas harum. Aliquid atque ratione. Temporibus sunt voluptas vel ea similique rem. Non molestias incidunt cupiditate exercitationem.",
-        "image": "https://loremflickr.com/320/240/restaurant,food/?random=11.355713784927278",
-        "viewCounts": 1,
-        "createdAt": "2021-10-10T07:19:42.000Z",
-        "updatedAt": "2021-11-13T13:34:33.000Z",
-        "CategoryId": 2,
-        "Category": {
-            "id": 2,
-            "name": "日本料理",
-            "createdAt": "2021-10-10T07:19:42.000Z",
-            "updatedAt": "2021-10-10T07:19:42.000Z"
-        },
-        "Comments": [
-            {
-                "id": 51,
-                "text": "Voluptatem sed adipisci beatae ex aut.",
-                "UserId": 1,
-                "RestaurantId": 1,
-                "createdAt": "2021-10-10T07:19:42.000Z",
-                "updatedAt": "2021-10-10T07:19:42.000Z",
-                "User": {
-                    "id": 1,
-                    "name": "root",
-                    "email": "root@example.com",
-                    "password": "$2a$10$AvHu1laIippZL3G.UJ08jOEMQZw/bRyFBonVfNCqABVvcO5ZHOqzS",
-                    "isAdmin": true,
-                    "image": null,
-                    "createdAt": "2021-10-10T07:19:42.000Z",
-                    "updatedAt": "2021-10-10T07:19:42.000Z"
-                }
-            },
-            {
-                "id": 1,
-                "text": "Enim deleniti officia asperiores ut quis.",
-                "UserId": 3,
-                "RestaurantId": 1,
-                "createdAt": "2021-10-10T07:19:42.000Z",
-                "updatedAt": "2021-10-10T07:19:42.000Z",
-                "User": {
-                    "id": 3,
-                    "name": "user2",
-                    "email": "user2@example.com",
-                    "password": "$2a$10$8BEcpQVgpsmUeysWWIYKTOzCA9lW.yrqBNSC7NBq/9g.kYcu.K2a2",
-                    "isAdmin": false,
-                    "image": null,
-                    "createdAt": "2021-10-10T07:19:42.000Z",
-                    "updatedAt": "2021-10-10T07:19:42.000Z"
-                }
-            },
-            {
-                "id": 101,
-                "text": "Est autem ipsam error dolorem nesciunt ducimus.",
-                "UserId": 3,
-                "RestaurantId": 1,
-                "createdAt": "2021-10-10T07:19:42.000Z",
-                "updatedAt": "2021-10-10T07:19:42.000Z",
-                "User": {
-                    "id": 3,
-                    "name": "user2",
-                    "email": "user2@example.com",
-                    "password": "$2a$10$8BEcpQVgpsmUeysWWIYKTOzCA9lW.yrqBNSC7NBq/9g.kYcu.K2a2",
-                    "isAdmin": false,
-                    "image": null,
-                    "createdAt": "2021-10-10T07:19:42.000Z",
-                    "updatedAt": "2021-10-10T07:19:42.000Z"
-                }
-            }
-        ]
-    }
-}
+import restaurantsAPI from "../apis/restaurants";
+import { Toast } from "../utils/helpers";
 
 export default {
-  name: 'RestaurantDashboard',
+  name: "RestaurantDashboard",
   data() {
     return {
       restaurant: {
         id: 1,
-        name: '',
-        categoryName: '',
+        name: "",
+        categoryName: "",
         commentsLength: 0,
-        viewCounts: 0
+        viewCounts: 0,
       },
-    }
+    };
   },
   created() {
-    const {id: restaurantId} = this.$route.params
-    this.fetchRestaurant(restaurantId)
+    const { id: restaurantId } = this.$route.params;
+    this.fetchRestaurant(restaurantId);
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params;
+    this.fetchRestaurant(restaurantId);
+    next()
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log(restaurantId)
-      const {id, name, Category, viewCounts, Comments} = dummyData.restaurant
-      this.restaurant = {id, name, categoryName: Category? Category.name: '未分類', viewCounts, commentsLength: Comments ? Comments.length : 0}
-    }
-  }
-}
+    async fetchRestaurant(restaurantId) {
+      try {
+        const response = await restaurantsAPI.getRestaurant({ restaurantId });
+        console.log(response);
+
+        const { id, name, Category, viewCounts, Comments } =
+          response.data.restaurant;
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          viewCounts,
+          commentsLength: Comments ? Comments.length : 0,
+        };
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: `無法存取餐廳資訊--${error.message}`,
+        });
+      }
+    },
+  },
+};
 </script>
